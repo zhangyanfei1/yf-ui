99c088c6849b15dcc8adc1ae54cfe160643f9001
zhangyanfei <zhangyanfei@csmail.citicbank.com>
2020-07-18 13:33:11 +0800

    提交

---
+++ examples/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <yf-button type="primary">123</yf-button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+
+</style>
--- examples/main.js
+++ examples/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import Yfui from '../packages/index'
 Vue.config.productionTip = false
-
+Vue.use(Yfui)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
